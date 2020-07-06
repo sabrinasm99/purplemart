397a86080f55e4dd3488f972de7086d903827cc9
Sabrina Sri Mulyani <sabrinasrimulyani@gmail.com>
2020-07-06 17:55:02 +0700

    uninstall

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,6 @@ import '../styles/tailwind.css';
 import '@brainhubeu/react-carousel/lib/style.css';
 import 'keen-slider/keen-slider.min.css'
 import '../components/Keen.css'
-// import '@fortawesome/fontawesome-free/js/fontawesome';
-// import '@fortawesome/fontawesome-free/js/solid';
-// import '@fortawesome/fontawesome-free/js/regular';
-// import '@fortawesome/fontawesome-free/js/brands';
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
