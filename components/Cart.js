23d8731191cdf13a6997f6647c44d00662623c01
Sabrina Sri Mulyani <sabrinasrimulyani@gmail.com>
2020-07-10 16:56:31 +0700

    toast

--- components/Cart.js
+++ components/Cart.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { addProduct, deleteProduct } from "./actions/cartAction";
+import { ToastContainer, toast, Zoom } from 'react-toastify';
 
 function Cart() {
   const cart = useSelector((state) => state.cart);
   const auth = useSelector((state) => state.auth);
+  const notify = () => toast('Hello Everyone');
   const onClickAdd = (id) => {
     if (auth.isAuthenticated) {
       addProduct(id);
@@ -43,7 +45,7 @@ function Cart() {
     }
   }
   return (
-    <div className="p-2 sm:p-0 sm:w-4/5 mx-auto mt-px62 sm:pt-5 text-sm sm:text-base">
+    <div className="px-2 pt-3 sm:px-0 sm:w-4/5 mx-auto mt-px62 sm:pt-5 text-sm sm:text-base">
       <div className="bg-white rounded-md text-center">
         <div className="flex bg-purple-800 rounded-t-md text-white p-1">
           <div className="w-1/4">Product Name</div>
@@ -59,9 +61,11 @@ function Cart() {
           </div>
         </div>
       </div>
-      <button className="border border-purple-800 bg-purple-800 p-1 mt-5 text-white focus:outline-none">
+      <button className="border border-purple-800 bg-purple-800 p-1 mt-5 text-white focus:outline-none"
+      onClick={notify}>
         Order Now
       </button>
+      <ToastContainer position='bottom-center' transition={Zoom} limit={1} />
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import '../styles/tailwind.css';
 import '@brainhubeu/react-carousel/lib/style.css';
 import 'keen-slider/keen-slider.min.css'
 import '../components/Keen.css'
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
